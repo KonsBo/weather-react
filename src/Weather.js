3ac78efe8ad2489d3b28464c015b1ab5551c8805
Konstantina Boua <85695279+KonsBo@users.noreply.github.com>
2023-07-02 18:35:18 +0300

    change loader to red

--- src/Weather.js
+++ src/Weather.js
@@ -18,7 +18,7 @@ export default function Weather(props) {
       height="80"
       width="80"
       radius="9"
-      color="white"
+      color="red"
       ariaLabel="loading"
       wrapperStyle
       wrapperClass
